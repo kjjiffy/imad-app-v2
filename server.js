21260c512d459d72651790141f4975d771c43756
kjjiffy <kjjiffy@gmail.com>
2017-02-18 19:36:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/article_one', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 });
 app.get('/article_two', function(req, res){
-    res.send('Article two is requested');
+    res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
 });
 app.get('/article_three', function(req, res){
-    res.send('Article three is requested');
+    res.sendFile(path.join(__dirname, 'ui', 'article_three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
