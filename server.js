daa0d526111856e94ffa3ae7e4a6b5ea456beb1c
kjjiffy <kjjiffy@gmail.com>
2017-02-20 00:49:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function(req, res){
     var articleName = req.params.articleName;
-    res.send(createTemplate(articleName));
+    res.send(createTemplate(article[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
