fcd08ba3e4f49579dc2b2677ecbe951d8bb1136e
kjjiffy <kjjiffy@gmail.com>
2017-02-22 22:53:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,9 @@ var element = document.getElementById('main_div');
 element.innerHTML = 'new value';
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
