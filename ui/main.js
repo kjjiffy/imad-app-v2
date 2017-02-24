5709c2d6ad6a3f001d6849917aeab47d1f167190
kjjiffy <kjjiffy@gmail.com>
2017-02-24 23:37:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@
 var counter = 0;
 var button = document.getElementById('counter');
 button.onclick = function() {
-    var interval = setInterval(moveRight, 100);
+    
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
