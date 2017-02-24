b0515ba07b4bb5f7f8f58c4c0de2dc33286f1e6e
kjjiffy <kjjiffy@gmail.com>
2017-02-24 23:37:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,9 @@
-console.log('Loaded!');
-var element = document.getElementById('main_div');
-element.innerHTML = 'new value';
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function() {
     var interval = setInterval(moveRight, 100);
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
