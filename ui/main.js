2b2aa17a86a0ec7a73dffce300ed4bea233911f9
kjjiffy <kjjiffy@gmail.com>
2017-02-25 19:16:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,3 +41,25 @@ submit.onclick = function() {
     request.open('GET', 'http://kjjiffy.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
+
+var commentInput = document.getElementById('comment');
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var comments = request.responseText;  
+                names = JSON.parse(comments);
+                var list = '';
+                for (var i = 0; i < comments.length; i++) {
+                    list += '<li>' + comments[i] + '</li>';
+                }
+                var ul = document.getElementById('commentlist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    var comment = commentInput.value;
+    request.open('GET', 'http://kjjiffy.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+    request.send(null);
