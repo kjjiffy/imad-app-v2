7a58c117c37f0e95a6b725fe5c87e76a8364c4f2
kjjiffy <kjjiffy@gmail.com>
2017-02-25 20:45:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,6 @@
 
 
 
-
-var nameInput = document.getElementById('name');
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-    console.log(window.location.toString());
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status === 200) {
-                var names = request.responseText;  
-                names = JSON.parse(names);
-                var list = '';
-                for (var i = 0; i < names.length; i++) {
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
-            }
-        }
-    };
-    var name = nameInput.value;
-    request.open('GET', window.location.toString() + 'submit-name?name=' + name, true);
-    request.send(null);
-};
-
 var submitcomment = document.getElementById('submit-comment');
 submitcomment.onclick = function() {
     var request = new XMLHttpRequest();
