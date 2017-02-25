1ad2195cbc8fdd3b89ad9c651c3f32d39a6ebef5
kjjiffy <kjjiffy@gmail.com>
2017-02-25 20:48:17 +0530

    Create discussions.js

---
+++ ui/discussions.js
@@ -0,0 +1,22 @@
+var submitcomment = document.getElementById('submit-comment');
+submitcomment.onclick = function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var comments = request.responseText;  
+                comments = JSON.parse(comments);
+                var list = '';
+                for (var i = 0; i < comments.length; i++) {
+                    list += '<li>' + comments[i] + '</li>';
+                }
+                var ulcomment = document.getElementById('commentlist');
+                ulcomment.innerHTML = list;
+            }
+        }
+    };
+    var commentInput = document.getElementById('comment');
+    var comment = commentInput.value;
+    request.open('GET', 'http://kjjiffy.imad.hasura-app.io/submit-comments?comment=' + comment, true);
+    request.send(null);
+};
