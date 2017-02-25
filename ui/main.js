b7c479b7313d8b728d221f5d2c3b5f7db1a230bd
kjjiffy <kjjiffy@gmail.com>
2017-02-25 20:44:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,5 @@
 
 
-var button = document.getElementById('counter');
-button.onclick = function() {
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET', 'http://kjjiffy.imad.hasura-app.io/counter', true);
-    request.send(null);
-    
-};
 
 
 var nameInput = document.getElementById('name');
