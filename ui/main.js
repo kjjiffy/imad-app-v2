18f470d99e693c32cd75c002e10d5a8bba053f6f
kjjiffy <kjjiffy@gmail.com>
2017-02-25 18:59:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,6 @@ button.onclick = function() {
 
 
 var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     var request = new XMLHttpRequest();
@@ -38,6 +37,7 @@ submit.onclick = function() {
             }
         }
     };
+    var name = nameInput.value;
     request.open('GET', 'http://kjjiffy.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
