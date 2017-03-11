5e9c57decfa7b041c7ce60a5d6c6917b92112240
kjjiffy <kjjiffy@gmail.com>
2017-03-11 23:05:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,13 +103,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter', function(req, res){
-    counter = counter + 1;
-    res.send(counter.toString());
-});
 var pool = new Pool(config);    
-
 app.get('/test-db', function(req, res){
    pool.query('SELECT * FROM test', function(req,res) {
        if(err) {
@@ -118,6 +112,12 @@ app.get('/test-db', function(req, res){
            res.send(JSON.stringify(result));
        }
    }) ;
+});
+
+var counter = 0;
+app.get('/counter', function(req, res){
+    counter = counter + 1;
+    res.send(counter.toString());
 });
 
 
