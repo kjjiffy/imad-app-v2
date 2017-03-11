71334f2095d8162d4b183ffb4f3dd5819b467871
kjjiffy <kjjiffy@gmail.com>
2017-03-12 01:09:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/submit-comments', function (req, res) {
 
 app.get('/articles/:articleName', function(req, res){
     
-    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err, result) {
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName.toString(), function(err, result) {
        if(err) {
            res.status(500).send(err.toString());
        } else {
