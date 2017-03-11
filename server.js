ae8bfaaa10db34058244f0dde1e2b84364510a1c
kjjiffy <kjjiffy@gmail.com>
2017-03-11 23:46:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database: 'kjjiffy',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password: 'db-kjjiffy-12922'
+    password: process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
