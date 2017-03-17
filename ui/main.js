a4fdb5d81fd70d1b941da56cf8a01a340cdee996
kjjiffy <kjjiffy@gmail.com>
2017-03-17 19:27:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,21 +7,21 @@ submit.onclick = function() {
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE) {
             if(request.status === 200) {
-                var names = request.responseText;  
-                names = JSON.parse(names);
-                var list = '';
-                for (var i = 0; i < names.length; i++) {
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ulcomment = document.getElementById('namelist');
-                ulcomment.innerHTML = list;
+                console.log('user logged in');
+                alert('logged in successfully');
+                
+            } else if (request.status === 403) {
+                alert('username/ password is incorrect');
             }
+        } else if (request.status === 500) {
+            alert('something went wrong in the server');
         }
     };
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     console.log(username);
     console.log(username);
-    request.open('POST', 'http://kjjiffy.imad.hasura-app.io/submit-comments?name=' + name, true);
+    request.setRequestHeader('Content-type', 'application/json');
+    request.open('POST', 'http://kjjiffy.imad.hasura-app.io/login' + name, true);
     request.send(JSON.stringify({username: username, password: password}));
 };
