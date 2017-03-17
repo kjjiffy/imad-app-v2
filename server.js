3270ec47e43090c448781cc854b68123bb643b83
kjjiffy <kjjiffy@gmail.com>
2017-03-17 12:57:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'kjjiffy', 
@@ -13,6 +14,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate (data) {
     var title = data.title;
@@ -71,6 +73,20 @@ app.get('/test-db', function(req, res){
            res.send(JSON.stringify(result.rows));
        }
    }) ;
+});
+
+app.post('/create-user', function(req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString =  hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
+       if(err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send("user successfully created with username: "+ username);
+       }
+   });
 });
 
 app.get('/hash/:input', function(req, res) {
